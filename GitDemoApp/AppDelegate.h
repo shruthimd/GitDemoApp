77fc767e6e4f20396d7d9fa94c11dd3ad5362684
shruthimd <shruthi.d@coppermobile.com>
2015-11-24 15:42:01 -0600

    Initial Commit

---
+++ GitDemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemoApp
+//
+//  Created by Shruthi MD  on 11/24/15.
+//  Copyright © 2015 Shruthi MD . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemoApp
+//
+//  Created by Shruthi MD  on 11/24/15.
+//  Copyright © 2015 Shruthi MD . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
